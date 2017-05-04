b422d1746c83b9606b365c903307d781c1098dd4
Ishai <yishaistrauss@gmail.com>
2017-05-04 09:00:34 -0500

    Adds js-stellar-dep.

--- src/resources/workers/stellar-server.js
+++ src/resources/workers/stellar-server.js
@@ -2,6 +2,8 @@
  * Created by istrauss on 3/16/2017.
  */
 
+import StellarSdk from 'js-stellar-sdk';
+
 export class StellarServer {
     constructor() {
         const server = new StellarSdk.Server(window.lupoex.horizon);
