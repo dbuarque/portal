b2b8037a09c602a669d67544c20fe5b61bd2a436
Ishai Strauss <yishaistrauss@gmail.com>
2017-12-03 12:28:44 -0600

    Fixes #73.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 sed "s/\%env\%/$1/g" dist/index.html.tmpl > dist/index.html
-aws s3 sync --delete --profile lupoex --acl public-read dist/ s3://$2
-aws cp --cache-control 'max-age=0' dist/index.html s3://$2/index.html
+aws s3 sync --profile lupoex --acl public-read --delete dist/ s3://$2
+aws s3 cp --profile lupoex --acl public-read --cache-control 'max-age=0' dist/index.html s3://$2/index.html
