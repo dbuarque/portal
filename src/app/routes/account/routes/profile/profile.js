60f5b9254d5cfc50acec01e30f846302761615d7
unknown <straussishai@gmail.com>
2018-01-18 11:14:55 -0600

    Fixes #78.

--- src/app/routes/account/routes/profile/profile.js
+++ src/app/routes/account/routes/profile/profile.js
@@ -3,22 +3,27 @@
  */
 
 import {inject} from 'aurelia-framework';
+import * as StellarSdk from 'stellar-sdk';
 import {Store, connected} from 'aurelia-redux-connect';
+import {TransactionService} from 'app-resources';
 import {UpdateAccountActionCreator} from '../../../../action-creators';
 
-@inject(Store, UpdateAccountActionCreator)
+@inject(Store, TransactionService, UpdateAccountActionCreator)
 export class Profile {
     @connected('myAccount')
     account;
 
     loading = 0;
 
+    editingProps = {};
+
     get refreshing() {
         return this.loading > 0;
     }
 
-    constructor(store, updateAccount) {
+    constructor(store, transactionService, updateAccount) {
         this.store = store;
+        this.transactionService = transactionService;
         this.updateAccount = updateAccount;
     }
 
@@ -32,5 +37,31 @@ export class Profile {
         this.accountData.refresh();
 
         this.loading--;
+    }
+
+    editProperty(prop) {
+        this[prop + 'New'] = this.account[prop];
+        this.editingProps[prop] = true;
+    }
+
+    cancelProperty(prop) {
+        this.editingProps[prop] = false;
+    }
+
+    async saveProperty(prop) {
+        try {
+            await this.transactionService.submit([
+                StellarSdk.Operation.setOptions({
+                    [prop]: this[prop + 'New']
+                })
+            ]);
+
+            await this.updateAccount.dispatch(this.account.accountId, {
+                force: true
+            });
+
+            this.editingProps[prop] = false;
+        }
+        catch (e) {}
     }
 }
